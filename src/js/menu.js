3362c05fe91f87839053eb15b639f5b1de0847d3
Lucky Prabath <lucky.prabath94@gmail.com>
2020-11-29 22:38:57 +0530

    add switch cases to menu.js

---
+++ src/js/menu.js
@@ -0,0 +1,22 @@
+$("#main-menu a").click(function(){
+    $("#main-menu a").removeClass("active");
+    $(this).addClass("active");
+    $(".content-wrapper").addClass("d-none");
+    switch ($(this).text().trim()){
+        case "Dashboard":
+            $("#dashboard").removeClass("d-none");
+            break;
+        case "Manage Customers":
+            $("#manage-customers").removeClass("d-none");
+            break;
+        case "Manage Items":
+            $("#manage-items").removeClass("d-none");
+            break;
+        case "Place Orders":
+            $("#place-orders").removeClass("d-none");
+            break;
+        case "Search Orders":
+            $("#search-orders").removeClass("d-none");
+            break;
+    }
+});
