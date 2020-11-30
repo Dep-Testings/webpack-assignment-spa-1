ecb2dd90bad14b66246fb11ba5ed55e7d905c86f
Lucky Prabath <lucky.prabath94@gmail.com>
2020-11-30 09:39:57 +0530

    buncle links and scripts in index.html to style.scss and index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+import '../node_modules/admin-lte/plugins/jquery/jquery.min.js';
+import '../node_modules/admin-lte/plugins/jquery-ui/jquery-ui.min.js';
+$.widget.bridge('uibutton', $.ui.button)
+import '../node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+import '../node_modules/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js';
+import '../node_modules/admin-lte/dist/js/adminlte.js';
+
 import './js/menu.js';
 import './scss/style.scss';
