d2ecf3c24be6f10ff902514d57dce5fab5b6d402
Lucky Prabath <lucky.prabath94@gmail.com>
2020-11-30 07:55:11 +0530

    set navigation using data attributes

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,31 +1,49 @@
-$("#main-menu a").click(function(event){
-    event.preventDefault;
+$("#main-menu a").click(function(){
     $("#main-menu a").removeClass("active");
     $(this).addClass("active");
     $(".content-wrapper").addClass("d-none");
 
     // ----method 01
-    switch ($(this).text().trim()){
-        case "Dashboard":
+    // switch ($(this).text().trim()){
+    //     case "Dashboard":
+    //         $("#dashboard").removeClass("d-none");
+    //         break;
+    //     case "Manage Customers":
+    //         $("#manage-customers").removeClass("d-none");
+    //         break;
+    //     case "Manage Items":
+    //         $("#manage-items").removeClass("d-none");
+    //         break;
+    //     case "Place Orders":
+    //         $("#place-orders").removeClass("d-none");
+    //         break;
+    //     case "Search Orders":
+    //         $("#search-orders").removeClass("d-none");
+    //         break;
+    //     default:
+    //         break;
+    
+    // }
+
+    //----method 02 --> using data attributes
+    switch ($(this).attr("data-menu")){
+        case "DASHBOARD":
             $("#dashboard").removeClass("d-none");
             break;
-        case "Manage Customers":
+        case "MANAGE_CUSTOMERS":
             $("#manage-customers").removeClass("d-none");
             break;
-        case "Manage Items":
+        case "MANAGE_ITEMS":
             $("#manage-items").removeClass("d-none");
             break;
-        case "Place Orders":
+        case "PLACE_ORDERS":
             $("#place-orders").removeClass("d-none");
             break;
-        case "Search Orders":
+        case "SEARCH_ORDERS":
             $("#search-orders").removeClass("d-none");
             break;
         default:
             break;
     
     }
-
-    //----method 02 --> using data attributes
-    
 });
